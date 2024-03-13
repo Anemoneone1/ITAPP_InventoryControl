7630d95be71ed633091380383045e0d5d2b5f338
DemyDeni <demydeni@gmail.com>
2024-03-13 18:25:39 +0100

    init

---
+++ src/main/java/com/itapp/inventorycontrol/ItappInventoryControlApplication.java
@@ -0,0 +1,13 @@
+package com.itapp.inventorycontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItappInventoryControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItappInventoryControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itapp/inventorycontrol/ItappInventoryControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itapp.inventorycontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItappInventoryControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
