fb07337639828fdba9b9387d6f8d5bc6232a03f8
DemyDeni <demydeni@gmail.com>
2024-06-06 00:52:08 +0200

    Update UserMapper

--- backend/src/main/java/com/itapp/inventorycontrol/mapper/UserMapper.java
+++ backend/src/main/java/com/itapp/inventorycontrol/mapper/UserMapper.java
@@ -1,14 +1,14 @@
 package com.itapp.inventorycontrol.mapper;
 
 import com.itapp.inventorycontrol.dto.request.EmployeeCreateRequest;
-import com.itapp.inventorycontrol.dto.request.UserCreateRequest;
+import com.itapp.inventorycontrol.dto.request.CompanyAndManagerCreateRequest;
 import com.itapp.inventorycontrol.dto.response.UserResponse;
 import com.itapp.inventorycontrol.entity.User;
 import org.mapstruct.Mapper;
 
 @Mapper
 public interface UserMapper {
-    User requestToUser(UserCreateRequest request);
+    User requestToUser(CompanyAndManagerCreateRequest request);
 
     User requestToUser(EmployeeCreateRequest request);
 
